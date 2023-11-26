65bff6d7d676cbbc489712dba9e191d7b9c364ee
Rosezey <149260398+Rosezey@users.noreply.github.com>
2023-11-26 17:41:19 +0300

    Update data.py

--- data.py
+++ data.py
@@ -3,6 +3,7 @@ import math
 health = 1
 battery = 1
 armour = 1
+pierce = 1
 repair = 1
 recharge = 1
 dmgelec = 1
@@ -13,7 +14,14 @@ reload = 1
 dex = 1
 dodge = (dex *= 3)
 alive = True
-
 turnres = 1
 turn = 1
 
+
+robotype = hound
+
+if robotype = hound;
+ pierce *= 1,5
+ health \= 2
+if robotype = cyborg;
+ 
