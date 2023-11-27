e4f0c2154c4b81fa332b76fd9d06c66ba6fc0a18
Rosezey <149260398+Rosezey@users.noreply.github.com>
2023-11-27 17:55:35 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 import math
 
-health = 1
-battery = 1
+health = 1.0
+battery = 1.0
 phyarmor = 1
 ammoarmor = 1
 elecarmor = 1
@@ -16,9 +16,9 @@ dex = 1
 accuracy = random.randint(0,100) += dex
 dodge = (dex *= 3)
 alive = True
-turnres = 1
+turnres = 1.0
 turn = 1
-exp = 0
+exp = 0.0
 level = 1
 if exp == 50 * level;
  level += 1
@@ -27,18 +27,25 @@ if exp == 50 * level;
 robotype = hound
 
 if robotype = hound;
- pierce *= 2
- health /= 1,5
- dex *= 1,5
- turnres += 1
- armor /= 2
- dmgphys *= 2
- dmgelec *= 2
- dmgammo /= 2
+ pierce *= 2.0
+ health /= 1.5
+ dex *= 1.5
+ turnres += 1.0
+ armor /= 2.0
+ dmgphys *= 2.0
+ dmgelec *= 2.0
+ dmgammo /= 2.0
  reload *= 2
- phyarmor /= 2
- ammoarmor /= 2
- elecarmor /= 2
- wifiarmor /= 2
+ phyarmor /= 2.0
+ ammoarmor /= 2.0
+ elecarmor /= 2.0
+ wifiarmor /= 2.0
 if robotype = cyborg;
- 
+ battery *= 1.5
+ phyarmor *= 1.0
+ pierce *= 1.2
+ dmgammo *=1.5
+ dmgwifi *= 2.0
+ dmgelec *=1.2
+ reload /= 1.5
+ dex *= 2.0
